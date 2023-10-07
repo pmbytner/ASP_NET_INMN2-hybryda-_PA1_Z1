237f73b14e0cf9dfd0a561ceb383d98b307432da
PMBytner <pawel.m.bytner@gmail.com>
2023-10-07 10:03:43 +0200

    Dodaj pliki projektów.

---
+++ ASP_NET_INMN2(hybryda)_PA1_Z1/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP_NET_INMN2_hybryda__PA1_Z1.Pages;
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
---
+++ ASP_NET_INMN2(hybryda)_PA1_Z1/Pages/Nowa.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP_NET_INMN2_hybryda__PA1_Z1.Pages
+{
+    public class NowaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
